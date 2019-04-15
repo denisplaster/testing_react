24e8cb6f0b7fb4c4d1ccbcfbb1378753a6cafaa7
denisplaster <35083974+denisplaster@users.noreply.github.com>
2019-04-14 20:04:37 -0500

    Updates

---
+++ precache-manifest.3e54d8c2c4b21908e3a2b7717dea198d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "27f40346f16eaebee630",
+    "url": "/denisplaster/testing_react.git/static/js/runtime~main.37ef7db7.js"
+  },
+  {
+    "revision": "b9108295e2afc00d2a99",
+    "url": "/denisplaster/testing_react.git/static/js/main.3dcb6156.chunk.js"
+  },
+  {
+    "revision": "4619a34bcb1711ae55d1",
+    "url": "/denisplaster/testing_react.git/static/js/2.1f5b0fb1.chunk.js"
+  },
+  {
+    "revision": "b9108295e2afc00d2a99",
+    "url": "/denisplaster/testing_react.git/static/css/main.ba60f863.chunk.css"
+  },
+  {
+    "revision": "d527d2a51de1918c80b043c541d95267",
+    "url": "/denisplaster/testing_react.git/index.html"
+  }
+];
